b05fc44164dbef3203757a7c2808a94d1d1b5407
Laksopan23 <laksopan2001@gmail.com>
2026-05-12 00:30:29 +0530

    style: remove header logo link and hover effects

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -49,9 +49,9 @@ export default function Navbar() {
       >
         <div className="max-w-6xl mx-auto px-6 h-16 flex items-center justify-between">
           {/* Logo */}
-          <a href="#hero" className="group">
-            <BrandLogo size={32} />
-          </a>
+          <div className="flex items-center">
+            <BrandLogo size={32} disableHover={true} />
+          </div>
 
           {/* Desktop Nav */}
           <div className="hidden md:flex items-center gap-1">
